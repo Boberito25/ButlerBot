13810d371418d8390ac1f49561c7c53d3dd18b97
vsunder <vsunder@Jeeves.(none)>
2013-11-14 11:19:24 -0500

    Body of A* implemented. no helpers

--- Butlerbot_Matlab/astar.m
+++ Butlerbot_Matlab/astar.m
@@ -1,4 +1,4 @@
-function [path] = astar(start, startconfig, target,costf, heuristic, jacobian, constraint, h)
+function [WS, path] = astar(start, startconfig, target,costf, heuristic, jacobian, constraint, h, tstep)
 	x = -30:h:30
 	y = -30:h:30
 	z = 0:h:30
@@ -7,7 +7,7 @@ function [path] = astar(start, startconfig, target,costf, heuristic, jacobian, c
 	ys = length(ys);
 	zs = length(zs);
 
-	WS = cell(xs,ys,zs,2);
+	WS = cell(xs,ys,zs,3);
 	[starts, Is] = find8closest(start, x, y, z);
 	Frontier = Is;
 	[targets, It] = find8closest(start, x, y, z);
@@ -15,31 +15,68 @@ function [path] = astar(start, startconfig, target,costf, heuristic, jacobian, c
 
 
 	% starts %
-	for i = 1:length(Frontier)
-		WS(Is(1,i),Is(2,i), Is(3,i),2) = costf(startconfig, start, translateCell(I(:,i),h))
-		WS(Is(1,i),Is(2,i), Is(3,i),1) = -1*ones(3,1);
+	[~, n] = size(Frontier);
+	for i = 1:n
+		next = translateCell(I(:,i),h);
+		newconfig = NewConfig(start,next,Jacobian,tstep) 
+		WS(Is(1,i),Is(2,i),Is(3,i),1)  = newconfig; %Configuration 
+		WS(Is(1,i),Is(2,i),Is(3,i),2)  = costf(next,start,newconfig, startconfig);
+		WS(Is(1,i),Is(2,i),Is(3,i),3)  = [-1;-1;-1]; %Previous
 	end	
-
+	currentstate = priority(Frontier, cost, heuristic, WS)
 	% loop %
-	currentstate = start
 	while(~targetreached(currentstate,It))
-		pstate = priority(Frontier, constf, heuristic)
-		NewFrontier = expandfrontier(WS, pstate);
-		Frontier = [Frontier, NewFrontier];
-		Frontier = find(Frontier ~= pstate);
-		
+		Idxs = expandfrontier(Frontier,WS, currentstate);
+		Frontier = [Frontier, Idxs];
+		% Expand frontier around prioritys
+		[~,n] = size(idxs);
+		for i = 1:n
+				current = translateCell(currentstate,h);
+				next = translateCell(Idxs(:,i),h);
+				newconfig = NewConfig(currents,next,Jacobian,tstep)
+				currentconfig = WS(currentstate(1),currentstate(2), currentstate(3), 1);
+				WS(Idxs(1,i), Idxs(2,i), Idxs(3,i), 1) = newconfig;
+				WS(Idxs(1,i), Idxs(2,i), Idxs(3,i), 2) = costf(next,current,newconfig, currentconfig);
+				WS(Idxs(1,i), Idxs(2,i), Idxs(3,i), 3) = currentstate; 
+			
+		end
+		%Filter current state from frontier
+		ridx = -1;
+		[~,n] = size(Frontier);
+		for i = 1:n
+			if(Frontier(:,i) == currentstate)
+				ridx = i;
+			end
+		end
+		if(ridx ~= n)
+			Frontier = [Frontier(:,1:ridx-1) Frontier(:,ridx+1 : n)];
+		else
+			Frontier = Frontier(:, 1:ridx-1);
+		end
+
+		%Update priority
+		currentstate = priority()
 	end
+
+	%back trace path
+	path = tracePath(WS, It);
+end
+
+function [newconfig] = NewConfig(curremt, next, Jacobian, timestep)
+end
+
+function [path] = tracepath(WS, It)
 end
 
 function [hasreached] = targetreached(state, Its)
 end
 
 %Expand the frontier at a given state
-function [Idxs] expandfrontier(WS, state)
+function [NewFrontier] expandfrontier(Frontier,WS, state)
 end
 
 % Returns the index of the cell required
-function [I] = priority(frontier, costf, heuristic) 
+function [I] = priority(frontier, costf, heuristic, WS) 
 end
 
 %Translates cell positions in to positions 
