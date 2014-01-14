2f7517c79794ea1ee5062e31077019b91138723f
vsunder <vsunder@Jeeves.(none)>
2014-01-14 13:53:26 -0500

    Basic torque rate planner ported to ros

--- rosbuild_ws/src/controllers/src/basic_arm_planner/basic_arm_planner.cpp
+++ rosbuild_ws/src/controllers/src/basic_arm_planner/basic_arm_planner.cpp
@@ -5,26 +5,26 @@
 #include "controllers/BasicArmMove.h"
 #include <vector>
 Basic_Arm_Planner::Basic_Arm_Planner(){}
-Basic_Arm_Planner::run(){}
-Basic_Arm_Planner::init(){}
+void Basic_Arm_Planner::run(){}
+void Basic_Arm_Planner::init(){}
 bool Basic_Arm_Planner::move(
 		  controllers::BasicArmMove::Request &req,
           controllers::BasicArmMove::Response &res){
 	Astar planner;
-	int[3] start = {req.theta1, req.theta2, req.theta3};
-	double[3] target = {req.x, req.z, req.alpha};
-	vector<PState*> path = planner.run(&start, &target);
+	int start[3] = {req.theta1, req.theta2, req.theta3};
+	double target[3] = {req.x, req.z, req.alpha};
+	vector<PState*> path = planner.run(start, target);
 	
 	//Construct paths
 	int len = path.size();
 
 	for(int i = 0; i < len; i++){
-		res.theta1s.push_back(ticks_to_radians(path.at(i).state[0], planner.numticks));
-		res.theta2s.push_back(ticks_to_radians(path.at(i).state[1], planner.numticks));
-		res.theta3s.push_back(ticks_to_radians(path.at(i).state[2], planner.numticks));
+		res.theta1s.push_back(tick_to_radians(path.at(i)->state[0], planner.numticks));
+		res.theta2s.push_back(tick_to_radians(path.at(i)->state[1], planner.numticks));
+		res.theta3s.push_back(tick_to_radians(path.at(i)->state[2], planner.numticks));
 	}
 
-	delete planner;
+	delete &planner;
 
 	return true;
 
@@ -32,6 +32,6 @@ bool Basic_Arm_Planner::move(
 int main(int argc, char **argv){
 	ros::init(argc, argv, "basic_arm_planner");
 	Basic_Arm_Planner c = Basic_Arm_Planner();
-	ros::ServiceServer planner = c.n.advertise("Basic_Arm_Mover", &Basic_Arm_Planner::move, &c);
+	ros::ServiceServer planner = c.n.advertiseService("Basic_Arm_Mover", &Basic_Arm_Planner::move, &c);
 	ros::spin();
 }
